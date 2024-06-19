b33a62e7ea23bab44595ca62756e09d9a2c190d8
jaycherd <jaycherd@gmail.com>
2024-06-19 14:50:08 -0700

    more data, now getting data from csv for quicker entry

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import json
+import csv
+from datetime import datetime
 
-FOR_SALE_ID = "x"
-PRICE_KEY = "price"
-SQ_FT_KEY = "sq_ft"
-BEDS_KEY = "beds"
-BATHS_KEY = "baths"
-PPSF_KEY = "price_per_sq_foot"
-PPSF_RANK_KEY = "price_per_sq_foot_rank"
+from utils.constants import HomeKeys as HK
+
 
 
 #pylint:disable=W0621
-def analyze_street_data(data):
+def analyze_data(data,for_sale_address,out_fname):
     sum_prices, sum_sq_ft, sum_beds, sum_baths, sum_ppsf = 0, 0, 0, 0, 0
     num_houses = 0
 
     for _ ,house in data.items():
-        if house.get("is_anomaly") == 1:
+        if house.get(HK.THROW_OUT_KEY) == 1:
             continue
-        house[PPSF_KEY] = round(house[PRICE_KEY] / house[SQ_FT_KEY], 0)
-        sum_prices += house[PRICE_KEY]
-        sum_sq_ft += house[SQ_FT_KEY]
-        sum_beds += house[BEDS_KEY]
-        sum_baths += house[BATHS_KEY]
-        sum_ppsf += house[PPSF_KEY]
+        house[HK.PPSF_KEY] = round(house[HK.PRICE_KEY] / house[HK.SQ_FT_KEY], 0)
+        sum_prices += house[HK.PRICE_KEY]
+        sum_sq_ft += house[HK.SQ_FT_KEY]
+        sum_beds += house[HK.BEDS_KEY]
+        sum_baths += house[HK.BATHS_KEY]
+        sum_ppsf += house[HK.PPSF_KEY]
         num_houses += 1
 
     avg_price = round(sum_prices / num_houses, 0)
@@ -31,54 +28,111 @@ def analyze_street_data(data):
     avg_baths = round(sum_baths / num_houses, 0)
     avg_ppsf = round(sum_ppsf / num_houses, 0)
 
-    print("Averages for this street:")
-    print(f"Average price: {avg_price}")
-    print(f"Average sq ft: {avg_sq_ft}")
-    print(f"Average beds : {avg_beds}")
-    print(f"Average baths: {avg_baths}")
-    print(f"Average ppsf : {avg_ppsf}")
+    with open(out_fname, 'w') as f:
+        f.write("Averages for this street:\n")
+        f.write(f"Average price: {avg_price}\n")
+        f.write(f"Average sq ft: {avg_sq_ft}\n")
+        f.write(f"Average beds : {avg_beds}\n")
+        f.write(f"Average baths: {avg_baths}\n")
+        f.write(f"Average ppsf : {avg_ppsf}\n")
 
-    print("\nComparing these to the house we want to buy:")
-    print(f"Average price: {round(data[FOR_SALE_ID][PRICE_KEY] - avg_price,0)}")
-    print(f"Average sq ft: {round(data[FOR_SALE_ID][SQ_FT_KEY] - avg_sq_ft,0)}")
-    print(f"Average beds : {round(data[FOR_SALE_ID][BEDS_KEY] - avg_beds,0)}")
-    print(f"Average baths: {round(data[FOR_SALE_ID][BATHS_KEY] - avg_baths,0)}")
-    print(f"Average ppsf : {round(data[FOR_SALE_ID][PPSF_KEY] - avg_ppsf,0)}")
+        f.write("\nComparing these to the house we want to buy:\n")
+        f.write(f"Average price: {round(data[for_sale_address][HK.PRICE_KEY] - avg_price, 0)}\n")
+        f.write(f"Average sq ft: {round(data[for_sale_address][HK.SQ_FT_KEY] - avg_sq_ft, 0)}\n")
+        f.write(f"Average beds : {round(data[for_sale_address][HK.BEDS_KEY] - avg_beds, 0)}\n")
+        f.write(f"Average baths: {round(data[for_sale_address][HK.BATHS_KEY] - avg_baths, 0)}\n")
+        f.write(f"Average ppsf : {round(data[for_sale_address][HK.PPSF_KEY] - avg_ppsf, 0)}\n")
 
-    sorted_items = sorted(
-                          ((k, v) for k, v in data.items() if v.get('is_anomaly') != 1),
-                          key=lambda x: x[1][PPSF_KEY]
-                         )
-    for ppsf_lo2hi_rank, (key,entry) in enumerate(sorted_items, start=1):
-        entry[PPSF_RANK_KEY] = ppsf_lo2hi_rank
-    sorted_data = {key: entry for key, entry in sorted_items}
-    
-    print(f"The price per sq foot of the for sale house has ranking {sorted_data[FOR_SALE_ID][PPSF_RANK_KEY]} out of {len(sorted_data)} houses")
-
-    print("\nThe houses with cheaper price per square foot are: ")
-    for id, house in sorted_data.items():
-        if id == FOR_SALE_ID:
-            break
-        print(house)
-    
-    print("\nSome theoretical prices based on houses with cheaper price per square foot")
-    for id, house in sorted_data.items():
-        if id == FOR_SALE_ID:
-            break
-        curr_ppsf = house[PPSF_KEY]
-        print(f"theoretical rank {house[PPSF_RANK_KEY]} of {len(sorted_data)}, if price per square foot was {curr_ppsf}," +
-              f" sale price would be {round(sorted_data[FOR_SALE_ID][SQ_FT_KEY] * curr_ppsf , 0)}")
+        sorted_items = sorted(
+                            ((k, v) for k, v in data.items() if v.get('is_anomaly') != 1),
+                            key=lambda x: x[1][HK.PPSF_KEY]
+                            )
+        for ppsf_lo2hi_rank, (key,entry) in enumerate(sorted_items, start=1):
+            entry[HK.PPSF_RANK_KEY] = ppsf_lo2hi_rank
+        sorted_data = {key: entry for key, entry in sorted_items}
         
-    print("\nIf we flip this house and raise the price per square foot to match the highest price per square foot vals on this street we'd get")
-    sorted_items.reverse()  # Reverse the sorted list
-    theoreticals_shown = 0
-    for key, entry in sorted_items:
-        if theoreticals_shown == 10:
-            break
-        curr_ppsf = entry[PPSF_KEY]
-        print(f"theoretical rank {entry[PPSF_RANK_KEY]} of {len(sorted_items)}, if price per square foot was {curr_ppsf}, " +
-            f"sale price would be {round(sorted_data[FOR_SALE_ID][SQ_FT_KEY] * curr_ppsf, 0)}")
-        theoreticals_shown += 1
+        f.write(f"The price per sq foot of the for sale house has ranking {sorted_data[for_sale_address][HK.PPSF_RANK_KEY]} out of {len(sorted_data)} houses\n")
+
+        f.write("\nThe houses with cheaper price per square foot are:\n")
+        for id, house in sorted_data.items():
+            if id == for_sale_address:
+                break
+            f.write(f"{house}\n")
+
+        f.write("\nall the houses collected for this neighborhood ranked by price per square foot: \n")
+        for id, house in sorted_data.items():
+            f.write(f"{house}\n")
+    
+        f.write("\nSome theoretical prices based on houses with cheaper price per square foot\n")
+        for id, house in sorted_data.items():
+            if id == for_sale_address:
+                break
+            curr_ppsf = house[HK.PPSF_KEY]
+            f.write(f"theoretical rank {house[HK.PPSF_RANK_KEY]} of {len(sorted_data)}, if price per square foot was {curr_ppsf}," +
+                f" sale price would be {round(sorted_data[for_sale_address][HK.SQ_FT_KEY] * curr_ppsf , 0)}\n")
+        
+        f.write("\nIf we flip this house and raise the price per square foot to match the highest prices per square foot we'd get\n")
+        sorted_items.reverse()  # Reverse the sorted list
+        theoreticals_shown = 0
+        for key, entry in sorted_items:
+            if theoreticals_shown == 15:
+                break
+            curr_ppsf = entry[HK.PPSF_KEY]
+            f.write(f"theoretical rank {entry[HK.PPSF_RANK_KEY]} of {len(sorted_items)}, if price per square foot was {curr_ppsf}, " +
+                f"sale price would be {round(sorted_data[for_sale_address][HK.SQ_FT_KEY] * curr_ppsf, 0)}\n")
+            theoreticals_shown += 1
+
+        recent_sales = sorted(
+                            ((k, v) for k, v in data.items()),
+                            key=lambda x: (x[1][HK.SOLD_DATE_KEY] != HK.NA, x[1][HK.SOLD_DATE_KEY] or datetime.min)
+                            )
+        recent_sales.reverse()
+        f.write(f"\nrecent sales:\n")
+        for address,house_data in recent_sales:
+            if house_data[HK.SOLD_DATE_KEY] == HK.NA:
+                break
+            f.write(f"{house_data}\n")
+
+        
+
+def json_to_dict(file_path):
+    # Load the JSON data from the file
+    with open(file_path, 'r',encoding="UTF-8") as json_file:
+        entries_list = json.load(json_file)
+
+    # Convert the list of dictionaries to a dictionary with `id` as keys
+    res = {entry['id']: entry for entry in entries_list}
+    return res
+
+def csv_to_dict(file_path):
+    data_dict = {}
+    
+    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            if row[HK.THROW_OUT_KEY] == '1':
+                continue
+            primary_key = row[HK.ADDRESS_KEY]
+            if row[HK.SOLD_PRICE_KEY] != '-1':
+                sold_date = datetime.strptime(row[HK.SOLD_DATE_KEY], '%m/%d/%Y')
+            else:
+                sold_date = HK.NA
+            data_dict[primary_key] = {
+                HK.ADDRESS_KEY: row[HK.ADDRESS_KEY],
+                HK.PRICE_KEY: float(row[HK.PRICE_KEY]),
+                HK.BEDS_KEY: float(row[HK.BEDS_KEY]),
+                HK.BATHS_KEY: float(row[HK.BATHS_KEY]),
+                HK.SQ_FT_KEY: float(row[HK.SQ_FT_KEY]),
+                HK.POOL_KEY: float(row[HK.POOL_KEY]),
+                HK.REMOD_SCORE_KEY: int(row[HK.REMOD_SCORE_KEY]),
+                HK.THROW_OUT_KEY: int(row[HK.THROW_OUT_KEY]),
+                HK.FOR_SALE_KEY: int(row[HK.FOR_SALE_KEY]),
+                HK.SOLD_PRICE_KEY: float(row[HK.SOLD_PRICE_KEY]),
+                HK.SOLD_DATE_KEY: sold_date
+            }
+    
+    return data_dict
 
 
 
@@ -86,20 +140,15 @@ def analyze_street_data(data):
     
 
 
+if __name__ == "__main__":
+    fname = "spanish_oaks_data.csv"
+    out_fname = "spanish_oaks_out_data.csv"
+    FOR_SALE_ADDRESS = "1901 Calle De Espana"
 
-# Load the JSON data from the file
-with open('street_vals.json', 'r',encoding="UTF-8") as json_file:
-    entries_list = json.load(json_file)
+    data = csv_to_dict(file_path=fname)
+    print(data)
 
-# Convert the list of dictionaries to a dictionary with `id` as keys
-street_houses_dict = {entry['id']: entry for entry in entries_list}
-print(street_houses_dict)
-
-
-
-
-
-analyze_street_data(data=street_houses_dict)
+    analyze_data(data,for_sale_address=FOR_SALE_ADDRESS,out_fname=out_fname)
 
 
 
---
+++ utils/constants.py
@@ -0,0 +1,19 @@
+
+class HomeKeys:
+    FOR_SALE_ID = "x"
+    ID_KEY = "id"
+    ADDRESS_KEY = "address"
+    PRICE_KEY = "price"
+    SQ_FT_KEY = "sq_ft"
+    BEDS_KEY = "beds"
+    BATHS_KEY = "baths"
+    POOL_KEY = "pool"
+    REMOD_SCORE_KEY = "subj_remodel_score"
+    THROW_OUT_KEY = "throw_out"
+    FOR_SALE_KEY = "for_sale"
+    SOLD_PRICE_KEY = "sold_price"
+    SOLD_DATE_KEY = "sold_date"
+
+    PPSF_KEY = "price_per_sq_foot"
+    PPSF_RANK_KEY = "price_per_sq_foot_rank"
+    NA = "NA"
